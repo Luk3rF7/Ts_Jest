04ee49677e61886f11ffa310db95025674588431
luk3rF7 <lkzeron@gmail.com>
2023-07-13 21:48:41 -0300

    costumer test1

--- src/05_DIP_DEPENDENCY_INVERSION_PRINCIPLE/test/costumer-test.spec.ts
+++ src/05_DIP_DEPENDENCY_INVERSION_PRINCIPLE/test/costumer-test.spec.ts
@@ -16,11 +16,11 @@ describe('should test Customer', () => {
   //getname,lastname,cpf
   describe('should test Customer', () => {
     const sut = createSutIndCustomer('Lucas', 'Felipe', '111.111');
-    expect(sut).toHaveProperty('firstName', 'Lucas');
-    expect(sut).toHaveProperty('lastName', 'Felipe');
-    expect(sut).toHaveProperty('cpf', '111.111');
+    expect(sut.getName()).toBe('Lucas Felipe');
+    expect(sut.getIDN()).toBe('111.111');
   });
 });
+//Enterprise
 const createSutEnterCustomer = (
   name: string,
   cnpj: string,
@@ -30,14 +30,13 @@ const createSutEnterCustomer = (
 //customer
 describe('should test Customer Enterprise', () => {
   const sut = createSutEnterCustomer('Lucas', '111.111');
-  expect(sut).toHaveProperty('firstName', 'Lucas');
-  expect(sut).toHaveProperty('lastName', 'Felipe');
+  expect(sut).toHaveProperty('name', 'Lucas');
+  expect(sut).toHaveProperty('cnpj', '111.111');
 
   //getname : name e cnpj
   describe('should test Customer Enterprise idn', () => {
-    const sut = createSutEnterCustomer('Lucas', '111.111');
-    expect(sut).toHaveProperty('firstName', 'Lucas');
-    expect(sut).toHaveProperty('lastName', 'Felipe');
-    expect(sut).toHaveProperty('cpf', '111.111');
+    const sut = createSutEnterCustomer('Lucas', '111');
+    expect(sut.getName).toBe('lucas');
+    expect(sut.getIDN).toBe('111');
   });
 });
